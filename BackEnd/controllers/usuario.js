e143294dd213cceccd3461229beea7668aec9524
Murilo Fragoso <mu_fragoso@hotmail.com>
2020-11-07 17:14:23 -0300

    Adicionando login, criptografia e validações no cadastro de usuario

--- BackEnd/controllers/usuario.js
+++ BackEnd/controllers/usuario.js
@@ -1,11 +1,56 @@
 const Usuario = require('../models/Usuario')
+const crypto = require('crypto');
 
 const controller = {}
 
 controller.novo = async (req, res) => {
+    let usuario = req.body;
+
+    //Validando senhas
+    if(usuario.senha != usuario.confirmarsenha){
+        res.status(400).send("Senhas não conferem")
+        return;
+    }
+
+    //criptografando senha
+    usuario.senha = criptografarSenha(usuario.senha);
+
+    //Validando se o e-mail ja foi cadastrado
     try{
-        await Usuario.create(req.body)
+        const validEmail = await Usuario.find({email: {$regex: usuario.email, $options: 'i'}});
+        if(validEmail.length > 0){
+            res.status(400).send("E-mail ja cadastrado")
+            return;
+        }
+
+        await Usuario.create(usuario)
         res.status(201).send('');
+    }
+    catch(erro){
+        console.log(erro)
+        res.status(500).send(erro)
+    }
+
+}
+
+controller.login = async (req, res) => {
+    let usuario = req.body;
+    usuario.senha = criptografarSenha(usuario.senha);
+    try{
+        const result = await Usuario.find({email: {$regex: usuario.email, $options: 'i'}});
+        if(result.length > 0){
+            let conta = result[0] // find retorna uma lista de usuarios, mas preciso apenas do primeiro, pois o email é unico
+            if(conta.senha == usuario.senha){
+                res.status(200).send(conta._id);
+            }
+            else{
+                res.status(400).send("E-mail ou senha incorretas")
+            }
+    
+        }else{
+            res.status(404).send("E-mail não encontrado");
+        }
+
     }
     catch(erro){
         console.log(erro)
@@ -76,6 +121,16 @@ controller.excluir = async(req, res) =>{
         console.log(erro)
         res.status(500).send(erro)
     }
+}
+
+function criptografarSenha(senha){
+    const algoritmo = "aes-192-cbc";
+    const chave = crypto.scryptSync(senha, 'salt', 24);
+
+    const cipher = crypto.createCipher(algoritmo, chave);
+    senha = cipher.update(senha, 'utf8', 'hex');
+    senha += cipher.final("hex");
+    return senha;
 }
 
 module.exports = controller 
--- BackEnd/routes/usuario.js
+++ BackEnd/routes/usuario.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/usuario')
 
 router.post('/', controller.novo)
+router.post('/login', controller.login)
 router.get('/', controller.listar)
 router.get('/:id', controller.obterUm)
 router.put('/', controller.atualizar)
